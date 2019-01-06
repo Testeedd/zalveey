b1ce55cf8f93f0a4260197e48585f2d06050dd16
AlvvxL <44138315+AlvvxL@users.noreply.github.com>
2019-01-06 23:53:39 +0700

    updated prefix cmd

--- commands/prefix.js
+++ commands/prefix.js
@@ -3,11 +3,14 @@ const Discord = require("discord.js")
 exports.run = (bot, message, args) => {
   if (!message.member.hasPermissions("MANAGE_SERVER")) return message.channel.send("Sorry, you need Manage Server permission to run this command")
   if (!args[1]) {
-    return message.channel.send("please enter a new bot prefix")// sucessfully set bot prefix to z;
+    return message.channel.send("please enter a new bot prefix")
   } else if (args[1] === "reset") {
     bot.servConf.delete(`config.${message.guild.id}.prefix`);
+    bot.servConf.delete(`prefix.${message.guild.id}`)
     message.channel.send("successfully reset bot prefix");
   } else if (args[1] === "set") {
+    if (!args[2]) return
+    bot.servConf.set(`prefix.${message.guild.id}.prefix`, args[2])
     bot.servConf.set(`config.${message.guild.id}.prefix`, args[2]);
     message.channel.send(`sucessfully set bot prefix to ${args[2]}`)
   }
