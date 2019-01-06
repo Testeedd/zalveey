328039c21019d3c86cfd61c7633583d19f357b2d
AlvvxL <44138315+AlvvxL@users.noreply.github.com>
2019-01-06 23:52:42 +0700

    ea

--- uptime.js
+++ uptime.js
@@ -2,13 +2,16 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
+let db = require("quick.db")
+let serv = new db.table("serverConfig")
+
 // Glitch Ping
 app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   console.log("Ping Received [" + new Date(Date.now()).toISOString().replace(/T/, ' ').replace(/\..+/, '') + "]");
     response.sendStatus(200);
-  });
+});
 
 // Request Listener
 var listener = app.listen(process.env.PORT, function() {
@@ -17,3 +20,14 @@ var listener = app.listen(process.env.PORT, function() {
 setInterval(() => {
     http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
+
+app.get('/api/prefix', function(request, response) {
+    response.send(serv.get("prefix"));
+});
+
+app.get('/api/prefix/:guildid', function(request, response) {
+  let guild = request.params.guildid
+  let guildprefix = serv.get(`prefix.${guild}`)
+  if (!guildprefix) return response.send({error:"no_prefix"})
+    response.send(serv.get("prefix"));
+});
